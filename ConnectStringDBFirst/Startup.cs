d12fb1c11f650c601ceb362b4cb60fa589254e65
matsurigoto <dog0416@gmail.com>
2016-04-05 21:07:15 +0800

    First commit

---
+++ ConnectStringDBFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConnectStringDBFirst.Startup))]
+namespace ConnectStringDBFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
